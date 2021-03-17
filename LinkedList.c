789a3992325d601f5e1a13a5d4c3ff55f87b4f1f
Replit user <replituser@example.com>
2021-03-17 12:07:43 +0000

    Added Node data structure and insert function

---
+++ LinkedList.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  struct Node* link;
+} Node;
+
+Node* head;
+
+Node* insert(Node* head, int x){
+  
+  Node* temp = (Node*)malloc(sizeof(Node));
+  temp->data = x;
+  temp->link = NULL;
+  if(head != NULL) temp->link = head;
+  head = temp;
+  return head;
+
+}
+
+void print(){
+  Node* temp = head;
+  printf("\nList is:");
+  while(temp != NULL){
+    printf(" %d", temp->data);
+    temp = temp->link;
+  }
+}
+
+int main(){
+  head = NULL;
+  printf("Enter the length of the list\n");
+  int n, i, x;
+  scanf("%d", &n);
+  printf("Initiate linked list with a list of numbers seperated by spaces\n");
+  for(i = 0; i< n; i++){
+    scanf("%d", &x);
+    insert(head, x);
+    
+  }
+  print();
+
+  return 0;
+} 
